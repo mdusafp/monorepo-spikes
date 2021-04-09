6edbec84f5a539a53afadd22469242e0fd58bee8
Vadim Maliutin <vmaliutin@griddynamics.com>
2021-04-09 18:26:10 +0300

    initial commit

---
+++ monorepo-lerna/packages/projects/lib/projects.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = projects;
+
+function projects() {
+    // TODO
+}
---
+++ monorepo-lerna/packages/secrets/lib/secrets.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = secrets;
+
+function secrets() {
+    // TODO
+}
